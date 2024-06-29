a8f10d370576e166a35cfa92b441768b7b800f31
kusimo <engr.kusimo>
2024-06-29 02:17:49 -0700

    added test html

--- frontend/static/js/config.js
+++ frontend/static/js/config.js
@@ -20,12 +20,13 @@ const local_config = {
 export const get_environment_url = () => {
     const url_variable = window.location.origin;
     if (url_variable === prod_config.url) {
-        console.log(url_variable, prod_config.name)
+        console.log(url_variable, prod_config.name, prod_config.api_url)
 
         return prod_config.api_url
     }
     else if (url_variable === test_config.url) {
         console.log(url_variable, prod_config.name)
+    
         return test_config.api_url
     }
     else {
