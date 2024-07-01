eed65d8879c4cac11d53e8a83e7f6b6b2116128a
itz-thevine <divine2christ@yahoo.com>
2024-07-01 16:33:13 +0100

    refresh and access token adjsutment

--- frontend/static/js/config.js
+++ frontend/static/js/config.js
@@ -1,32 +1,34 @@
 const prod_config = {
     name: "production",
     url: "http://app.clickbuy.ai",
-    api_url: "http://app.clickbuy.ai/prod-api/api/v1"
+    api_url: "http://app.clickbuy.ai/prod-api/api/v1",
+    login_redirect: "/login"
 }
 
 const test_config = {
     name: "test",
     url: "http://app.clickbuy.ai/test",
-    api_url: "http://app.clickbuy.ai/test-api/api/v1"
+    api_url: "http://app.clickbuy.ai/test-api/api/v1",
+    login_redirect: "/test/login"
 }
 
 
 const local_config = {
     name: "localhost",
     url: "http://localhost:8003",
-    api_url: "http://app.clickbuy.ai/test-api/api/v1"
+    api_url: "http://app.clickbuy.ai/test-api/api/v1",
+    login_redirect: "/test/login"
 }
 
 export const get_environment_url = () => {
+    const path = window.location.pathname;
     const url_variable = window.location.origin;
-
-    if (url_variable === prod_config.url) {
-        return prod_config
-    }
-    else if (url_variable === test_config.url) {
+    
+    if (path.startsWith('/test')) {
         return test_config
-    }
-    else {
+    } else if (url_variable === prod_config.url) {
+        return prod_config
+    } else {
         return local_config
     }
 }
