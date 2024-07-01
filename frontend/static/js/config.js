161f6a76ad4c0add7925713eb21dd02dfd6991d3
itz-thevine <divine2christ@yahoo.com>
2024-07-01 18:34:21 +0100

    refresh and access token adjsutment

--- frontend/static/js/config.js
+++ frontend/static/js/config.js
@@ -24,11 +24,15 @@ export const get_environment_url = () => {
     const path = window.location.pathname;
     const url_variable = window.location.origin;
     
-    if (path.startsWith('/test')) {
+    console.log("path", path)
+
+    let environment = url_variable + path
+    if (environment.includes('/test')) {
         return test_config
     } else if (url_variable === prod_config.url) {
         return prod_config
-    } else {
+    } 
+ else {
         return local_config
     }
 }
